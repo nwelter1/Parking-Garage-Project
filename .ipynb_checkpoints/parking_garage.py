1a94e5169bfb564c87fa440da26d36d27d4e4415
snavarrete1 <snavarrete3077@gmail.com>
2020-07-24 16:01:16 -0500

    Final commit

--- .ipynb_checkpoints/parking_garage.py
+++ .ipynb_checkpoints/parking_garage.py
@@ -1,38 +1,70 @@
-class Garage():
+from IPython.display import clear_output
+
+
+class Garage(): 
     def __init__(self,tickets,parkingSpaces,currentTicket):
         self.tickets = tickets
         self.parkingSpaces = parkingSpaces
         self.currentTicket = currentTicket
     
     def takeTicket(self):
-        self.currentTicket[self.ticket[-1]] = ''
-        del self.tickets[-1]
-        del self.parkingSpaces[-1]
-        print('Please take your ticket...')
-        
-
-
-
-    def payForParking():
-        response = input('Please enter your ticket number:')
-        if self.currentTicket[response] == '':
-            print('Please pay for your ticket...')
-            payment = input('Press any key to pay..')
-            self.currentTicket[response] = 'paid'
-            print('Thanks for parking! You have 15 minutes to exit.')
+        clear_output()
+        if self.tickets == []:
+            print('Sorry garage is currently full')
         else:
-            print('Your ticket is already paid! You have 15 minutes to exit the lot.')
+            self.currentTicket[self.tickets[0]] = ''
+            print(f'Please take ticket {self.tickets[0]}')
+            del self.tickets[0]
+            del self.parkingSpaces[0]
+            print(self.currentTicket)
+            print(self.tickets)
+            
+            
         
-        
-        
-        #self.tickets.append(int([-1]+1))
-        #self.parkingSpaces.append(int[-1]+1)
 
-
-
-
-
-
+    def payForParking(self):
+        clear_output()
+        response = int(input('Please enter your ticket number:'))
+        if response < 1 or response > 10:
+            print('Invalid ticket number')
+        else:
+            if self.currentTicket[response] == '':
+                print('Please pay for your ticket...')
+                payment = input('Press any key to pay..')
+                self.currentTicket[response] = 'paid'
+                print(self.currentTicket)
+            else:
+                print('you have 15 min 2 leave')
+                
+            
     def leaveGarage(self):
-
-        
+        clear_output()
+        response = int(input('Please scan your ticket:'))
+        if self.currentTicket[response] == 'paid':
+            print('Thank you have a nice day')
+            self.tickets.insert(0, response)
+            self.parkingSpaces.insert(0, response)
+            del self.currentTicket[response]
+            
+        else: 
+            print('Please pay before exiting')
+            
+            
+def runGarage():
+    p_garage = Garage([1,2, 3, 4, 5,6,7, 8, 9, 10],[1,2, 3, 4, 5,6,7, 8, 9, 10],{})
+    response = input('Welcome to Wabash Parking, would you like to park your cark? Yes or No?')
+    if response == 'No':
+        print('Okay, have a nice day!')
+    elif response == 'Yes':
+        p_garage.takeTicket()
+    while True:
+        response = input('What would you like to do? Park, Pay, Leave?')
+        if response.lower() == 'park': 
+            p_garage.takeTicket()
+        elif response.lower() == 'pay':
+            p_garage.payForParking()
+        elif response.lower() == 'leave':
+            p_garage.leaveGarage()
+            break
+    
+runGarage() 
