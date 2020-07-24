cf414bd4952e87142bd706b344a83ff703cb5f32
nwelter1 <nwelter1@gmail.com>
2020-07-24 12:20:38 -0500

    First commit w/ 2 methods complete

---
+++ .ipynb_checkpoints/parking_garage.py
@@ -0,0 +1,37 @@
+class Garage():
+    def __init__(self,tickets,parkingSpaces,currentTicket):
+        self.tickets = tickets
+        self.parkingSpaces = parkingSpaces
+        self.currentTicket = currentTicket
+    
+    def takeTicket(self):
+        del self.tickets[-1]
+        del self.parkingSpaces[-1]
+        print('Please take your ticket...')
+        self.currentTicket[self.ticket[-1]] = ''
+
+
+
+    def payForParking():
+        response = input('Please enter your ticket number:')
+        if self.currentTicket[response] == '':
+            print('Please pay for your ticket...')
+            payment = input('Press any key to pay..')
+            self.currentTicket[response] = 'paid'
+            print('Thanks for parking! You have 15 minutes to exit.')
+        else:
+            print('Your ticket is already paid! You have 15 minutes to exit the lot.')
+        
+        
+        
+        #self.tickets.append(int([-1]+1))
+        #self.parkingSpaces.append(int[-1]+1)
+
+
+
+
+
+
+    def leaveGarage(self):
+
+        
